ee2218bcaeccbe096e63834ff9a0a87ad0fb5124
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-04-15 18:15:30 +1000

    v1.1 - Only 1440p

---
+++ app.js
@@ -0,0 +1,25 @@
+// import Masonry from 'masonry-layout';
+
+
+window.onload = () => {
+    const grid = document.querySelector('.masonry');
+
+    const masonry = new Masonry(grid, {
+        itemSelector: '.image-container',
+        gutter: 40,
+        horizontalOrder: true,
+        originTop: true,
+        fitWidth: true
+    });
+};
+
+let imgCont = document.querySelectorAll('.image-container');
+
+if (screen.width >= 1440 && screen.width < 1790) {
+
+
+setTimeout( function() {
+    document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '1050px';
+    document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '998px';
+}, 500)
+}
