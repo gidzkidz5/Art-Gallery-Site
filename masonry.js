9f7b419f820e5f04b66c77947e8daa8e4275a71d
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-04-23 13:17:49 +1000

    v1.22d

--- masonry.js
+++ masonry.js
@@ -16,19 +16,19 @@ window.onload = () => {
 let imgCont = document.querySelectorAll('.image-container');
 
 
-
-if (screen.width >= 1440) {
 setTimeout( function() {
+if (screen.width >= 1440) {
+// setTimeout( function() {
     document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '1050px';
     document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '998px';
 
-}, 550)
-}
+// }, 500)
+}}, 500)
 
 if (screen.width >= 767 && screen.width < 810) {
     setTimeout( function() {
         document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '364px';
         document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '2459px';
     
-    }, 550)
+    }, 500)
 }
