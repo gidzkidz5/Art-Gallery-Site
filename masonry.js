7991c2f340cafb2c19fdffc5a84e2b3fa10815cc
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-04-23 13:33:33 +1000

    v1.22

--- masonry.js
+++ masonry.js
@@ -13,22 +13,22 @@ window.onload = () => {
     });
 };
 
-let imgCont = document.querySelectorAll('.image-container');
+// let imgCont = document.querySelectorAll('.image-container');
 
 
-setTimeout( function() {
-if (screen.width >= 1440) {
 // setTimeout( function() {
-    document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '1050px';
-    document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '998px';
+// if (screen.width >= 1440) {
+// // setTimeout( function() {
+//     document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '1050px';
+//     document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '998px';
 
-// }, 500)
-}}, 500)
+// // }, 500)
+// }}, 500)
 
-if (screen.width >= 767 && screen.width < 810) {
-    setTimeout( function() {
-        document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '364px';
-        document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '2459px';
+// if (screen.width >= 767 && screen.width < 810) {
+//     setTimeout( function() {
+//         document.querySelectorAll('.image-container')[imgCont.length -1].style.left = '364px';
+//         document.querySelectorAll('.image-container')[imgCont.length -1].style.top = '2459px';
     
-    }, 500)
-}
+//     }, 500)
+// }
